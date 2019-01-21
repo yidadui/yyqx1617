8bf23bcc3885ca716225c90470a6133fa783bc68
wz <992590096@qq.com>
2019-01-21 15:07:27 +0800

    ddd

--- pro/src/App.js
+++ pro/src/App.js
@@ -14,6 +14,7 @@ class App extends Component {
             className="App-link"
             href="https://reactjs.org"
             target="_blank"
+            
             rel="noopener noreferrer"
           >
             Learn React
