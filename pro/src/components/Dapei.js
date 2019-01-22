a8e817005f93451b4fd083b64f4709091b17bef4
Gazer <1040274805@qq.com>
2019-01-22 17:05:30 +0800

    dapei

--- pro/src/components/Dapei.js
+++ pro/src/components/Dapei.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router,Route,Link,NavLink,Redirect,Switch} from 'react-router-dom';
 import Footer from './Footer';
+import '../styles/Dapei.css';
 class Dapei extends Component {
   render() {
     return (
       <div className="Dapei">
-      	
+      <header className="ld-head">
+        <p>搭配</p>
+      </header>
+      <section className="ld-sect">
+      </section>
         <Footer/>
       </div>
     );
